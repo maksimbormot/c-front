890b03c082ffe3bb8f1c61fc3f837f09ddfcfb4c
Tom Allen <tom.allen@metable.co>
2017-11-18 19:34:26 +0000

    update locations

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 console.log("ENV: " + process.env.NODE_ENV);
 if(process.env.NODE_ENV == 'development') {
 	module.exports.apiUri = "http://localhost:8081"
-} else if(process.env.NODE_ENV == 'production') {
-	// Change once production environment is set up
-	module.exports.apiUri = "https://api.curveroyaltysystems.com"
-} else {
+} else if(process.env.NODE_ENV == 'staging') {
 	module.exports.apiUri = "https://staging-api.curveroyaltysystems.com"
+} else {
+	module.exports.apiUri = "https://api.curveroyaltysystems.com"
 }
 
